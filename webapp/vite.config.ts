cf70f81ffd341704c53c9415cc9eb2609c667f7e
Thierry Philippe <lord@lordfpx.com>
2025-10-14 22:43:34 +0200

    Merge origin/main into work

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 const repoBase = '/img2img2/';
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
   // Use the repository sub-path when building the static bundle for GitHub Pages
