1df7507fc5de8c353edcd14bb656b3256ff644e0
lordfpx <lord@lordfpx.com>
2025-10-16 13:53:27 +0200

    1er commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 const repoBase = '/img2img2/';
 
